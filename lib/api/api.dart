3dcc65a760fefc9b1c5ac13c70b9b54544df95cc
JWHAPO <kim2101714@gmail.com>
2020-04-01 16:55:19 +0900

    add getUser api sample

--- lib/api/api.dart
+++ lib/api/api.dart
@@ -3,8 +3,44 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'dart:async';
 
+import 'package:oraeapp/domain/user.dart';
+
 class Api{
 
   static const BASE_URL = 'http://api.test.com';
+
+  static String getUrlUserList() => BASE_URL+'/user';
+
+
+
+  static Future<List<User>> getUsers() async {
+    String _jsonResponse = "";
+    try{
+      http.Response response =
+      await http.post(Uri.encodeFull(getUrlUserList()), headers: {"Content-type": "application/json", });
+
+      if(response.statusCode == 200){
+        _jsonResponse = utf8.decode(response.bodyBytes);
+
+        print('Json:$_jsonResponse');
+
+        Map<String, dynamic> json = jsonDecode(_jsonResponse);
+
+        Users users = Users.fromJson(json);
+
+        return users.users;
+      }else{
+        //To Do 에러처리
+        return null;
+      }
+
+    } on TimeoutException catch (e) {
+      print('TimeoutException$e');
+      return null;
+    } on Error catch (e){
+      print('Error$e');
+      return null;
+    }
+  }
 
 }
---
+++ lib/domain/user.g.dart
@@ -0,0 +1,64 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'user.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+User _$UserFromJson(Map<String, dynamic> json) {
+  return User(
+    json['userId'] as int,
+    json['type'] as String,
+    json['id'] as String,
+    json['password'] as String,
+    json['name'] as String,
+    json['tel'] as String,
+    json['isUse'] as String,
+    json['isAlarm'] as String,
+    json['zipCode'] as String,
+    json['addrMain'] as String,
+    json['addrSub'] as String,
+    json['email'] as String,
+    json['birthday'] as String,
+    json['gender'] as String,
+    json['profileUrl'] as String,
+    json['token'] as String,
+    json['refreshToken'] as String,
+    json['pushToken'] as String,
+  );
+}
+
+Map<String, dynamic> _$UserToJson(User instance) => <String, dynamic>{
+      'userId': instance.userId,
+      'type': instance.type,
+      'id': instance.id,
+      'password': instance.password,
+      'name': instance.name,
+      'tel': instance.tel,
+      'isUse': instance.isUse,
+      'isAlarm': instance.isAlarm,
+      'zipCode': instance.zipCode,
+      'addrMain': instance.addrMain,
+      'addrSub': instance.addrSub,
+      'email': instance.email,
+      'birthday': instance.birthday,
+      'gender': instance.gender,
+      'profileUrl': instance.profileUrl,
+      'token': instance.token,
+      'refreshToken': instance.refreshToken,
+      'pushToken': instance.pushToken,
+    };
+
+Users _$UsersFromJson(Map<String, dynamic> json) {
+  return Users(
+    (json['users'] as List)
+        ?.map(
+            (e) => e == null ? null : User.fromJson(e as Map<String, dynamic>))
+        ?.toList(),
+  );
+}
+
+Map<String, dynamic> _$UsersToJson(Users instance) => <String, dynamic>{
+      'users': instance.users,
+    };

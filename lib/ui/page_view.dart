51c0e33b47fb6cb0eba2c2ab805cf2272bee3063
JWHAPO <kim2101714@gmail.com>
2020-04-21 08:35:28 +0900

    add viewPager indicator

--- lib/ui/page_view.dart
+++ lib/ui/page_view.dart
@@ -1,23 +1,136 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 class PageViewTest extends StatelessWidget {
+
+  final _controller = PageController();
+  static const _kDuration = const Duration(milliseconds: 300);
+  static const _kCurve = Curves.ease;
+  final _kArrowColor = Colors.black.withOpacity(0.8);
+
+  final List<Widget> _pages = <Widget>[
+    ConstrainedBox(
+      constraints: const BoxConstraints.expand(),
+      child: Container(
+        color: Colors.pink,
+        child: Text('Page1'),
+      ),
+    ),
+    ConstrainedBox(
+      constraints: const BoxConstraints.expand(),
+      child: Container(
+        color: Colors.cyan,
+        child: Text('Page2'),
+      ),
+    ),
+    ConstrainedBox(
+      constraints: const BoxConstraints.expand(),
+      child: Container(
+        color: Colors.deepPurple,
+        child: Text('Page3'),
+      ),
+    )
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return PageView(
-      children: <Widget>[
-        Container(
-          color: Colors.pink,
-          child: Text('Page1'),
+    return IconTheme(
+      data: IconThemeData(color: _kArrowColor),
+      child: Stack(
+        children: <Widget>[
+          PageView.builder(
+            physics: AlwaysScrollableScrollPhysics(),
+            controller: _controller,
+            itemBuilder: (BuildContext context, int index){
+              return _pages[index % _pages.length];
+            },
+          ),
+          Positioned(
+            bottom: 0.0,
+            left: 0.0,
+            right: 0.0,
+            child: Container(
+              color: Colors.grey[800].withOpacity(0.5),
+              padding: EdgeInsets.all(20.0),
+              child: Center(
+                child: DotsIndicator(
+                  controller: _controller,
+                  itemCount: _pages.length,
+                  onPageSelected: (int page){
+                    _controller.animateToPage(page, duration: _kDuration, curve: _kCurve);
+                  },
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+/// An indicator showing the currently selected page of a PageController
+class DotsIndicator extends AnimatedWidget {
+  DotsIndicator({
+    this.controller,
+    this.itemCount,
+    this.onPageSelected,
+    this.color: Colors.white,
+  }) : super(listenable: controller);
+
+  /// The PageController that this DotsIndicator is representing.
+  final PageController controller;
+
+  /// The number of items managed by the PageController
+  final int itemCount;
+
+  /// Called when a dot is tapped
+  final ValueChanged<int> onPageSelected;
+
+  /// The color of the dots.
+  ///
+  /// Defaults to `Colors.white`.
+  final Color color;
+
+  // The base size of the dots
+  static const double _kDotSize = 8.0;
+
+  // The increase in the size of the selected dot
+  static const double _kMaxZoom = 2.0;
+
+  // The distance between the center of each dot
+  static const double _kDotSpacing = 25.0;
+
+  Widget _buildDot(int index) {
+    double selectedness = Curves.easeOut.transform(
+      max(
+        0.0,
+        1.0 - ((controller.page ?? controller.initialPage) - index).abs(),
+      ),
+    );
+    double zoom = 1.0 + (_kMaxZoom - 1.0) * selectedness;
+    return new Container(
+      width: _kDotSpacing,
+      child: new Center(
+        child: new Material(
+          color: color,
+          type: MaterialType.circle,
+          child: new Container(
+            width: _kDotSize * zoom,
+            height: _kDotSize * zoom,
+            child: new InkWell(
+              onTap: () => onPageSelected(index),
+            ),
+          ),
         ),
-        Container(
-          color: Colors.cyan,
-          child: Text('Page2'),
-        ),
-        Container(
-          color: Colors.deepPurple,
-          child: Text('Page3'),
-        ),
-      ],
+      ),
+    );
+  }
+
+  Widget build(BuildContext context) {
+    return new Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: new List<Widget>.generate(itemCount, _buildDot),
     );
   }
 }

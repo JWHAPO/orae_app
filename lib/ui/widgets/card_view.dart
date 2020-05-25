e396ab036afdb92dcae1873e975ac73a9ba95086
JWHAPO <kim2101714@gmail.com>
2020-05-25 23:26:54 +0900

    add page controller

--- lib/ui/widgets/card_view.dart
+++ lib/ui/widgets/card_view.dart
@@ -8,6 +8,20 @@ class SlidingCardsView extends StatefulWidget {
 }
 
 class _SlidingCardsViewState extends State<SlidingCardsView> {
+  PageController pageController;
+
+  @override
+  void initState() {
+    super.initState();
+    pageController = PageController(viewportFraction: 0.8);
+  }
+
+  @override
+  void dispose() {
+    pageController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return SizedBox(
